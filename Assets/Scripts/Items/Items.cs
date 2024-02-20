12e4f85cfa0e2a639a656e273c6b2df078e7022a
alesandrakkk <aliasandrakk@gmail.com>
2024-02-20 17:04:20 +0100

    something changed

---
+++ Assets/Scripts/Items/Items.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Items
+{
+    public class Items : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
